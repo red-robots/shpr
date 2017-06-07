a973e8b1507cc71fb23a9487ad2c40e869a6755c
Austin Crane <austincrane@gmail.com>
2017-06-07 10:10:34 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -939,3 +939,8 @@ function remove_wp_logo( $wp_admin_bar ) {
 	$wp_admin_bar->remove_node( 'wp-logo' );
 	$wp_admin_bar->remove_node( 'site-name' );
 }
+
+/*-------------------------------------
+	Adds Options page for ACF.
+---------------------------------------*/
+if( function_exists('acf_add_options_page') ) {acf_add_options_page();}
